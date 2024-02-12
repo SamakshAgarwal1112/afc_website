e2e9a3b3e849b3021b0737482157e9ca259f194f
Samaksh Agarwal <agarwalsamaksh11@gmail.com>
2024-02-13 01:48:09 +0530

    Changed form link and text and removed assets

--- src/components/Forms/index.jsx
+++ src/components/Forms/index.jsx
@@ -30,10 +30,10 @@ function Forms(){
                 fontWeight={400}
                 color={"#fcf9f9"}
                 >
-                {"Fill out the form to be a part of the AFC technical team. This is an open-book exam (you can use the internet or refer to any book but you should be able to explain the situation when asked). The interview for selection will be scheduled. It will be informed through the mail. The deadline for submission is mentioned in the form itself. In case of query, you may contact us."}
+                {"Fill out the following form to be a part of AFC. Details can be found in the form itself. In case of query, you may contact us."}
                 </Text>
                 <Link 
-                href="https://forms.gle/nZXmP4vD6peX7J8Q9" 
+                href="https://forms.gle/vgXKXmzV5S3PkFLQ9" 
                 isExternal
                 fontFamily={"Poppins"}
                 fontSize={{base:"4.5vw",md:"1.5rem",lg:"2rem",'2xl':"3.25vw"}}
